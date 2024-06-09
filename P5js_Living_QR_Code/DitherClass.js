c4867c933567712e3058d95367a9d0d4228a23ad
twisst <jaap@eerlijkemedia.nl>
2024-06-09 20:30:55 +0200

    Adding P5js version

---
+++ P5js_Living_QR_Code/DitherClass.js
@@ -0,0 +1,180 @@
+class Dither {
+  constructor() {
+    
+    colorMode(HSB, 255);
+    this.temp = null;
+    this.result = null;
+    this.src = null;
+    this.p = null;
+    this.mode = 0; // 0 floyd, 1 bayer, 2 atkinson, 3 random
+
+    // Bayer matrix
+    this.matrix = [
+      [1, 9, 3, 11],
+      [13, 5, 15, 7],
+      [4, 12, 2, 10],
+      [16, 8, 14, 6],
+    ];
+    this.mratio = 1.0 / 17;
+    this.mfactor = 255.0 / 5;
+  }
+
+  feed(img) {
+    
+    this.src = img;
+    this.src.loadPixels();
+    this.temp = [];
+
+    // put all brightness values of the source frame into an array
+    for (let y = 0; y < 87; y++) {
+      for (let x = 0; x < 87; x++) {
+        var index = (x + y * 87)*4;
+        var c = color(this.src.pixels[index + 0], this.src.pixels[index + 1], this.src.pixels[index + 2]);
+        var b = brightness(c);
+        this.temp.push(b);
+      }
+    }
+
+    // same, but with get()
+    // for (let y = 0; y < 87; y++) {
+    //   for (let x = 0; x < 87; x++) {
+    //     // let srcpixel = this.src.get(x, y);
+    //     let srcpixel = this.src.pixels[x + y * 87];
+    //     this.temp.push(brightness(srcpixel));
+    //   }
+    // }
+
+  }
+
+  dither(mode) {
+    this.mode = mode;
+    if (this.mode === 0) return this.floydSteinberg();
+    else if (this.mode === 1) return this.bayer();
+    else if (this.mode === 2) return this.atkinson();
+    else if (this.mode === 3) return this.rand();
+    return null;
+  }
+
+  floydSteinberg() {
+
+    this.result = [];
+
+    for (let y = 0; y < 87; y++) {
+      for (let x = 0; x < 87; x++) {
+        let oldpixel = this.temp[x + y * 87];
+        let newpixel = this.makeBlackorWhite(oldpixel);
+        let quant_error = brightness(oldpixel) - brightness(color(newpixel));
+
+        this.temp[x + y * 87] = newpixel;
+
+        this.temp[x+1 + y * 87] = this.temp[x + 1 + y * 87] + (7.0 / 16) * quant_error;
+        this.temp[x-1 + (y+1) * 87] = this.temp[x - 1 + (y + 1) * 87] + (3.0 / 16) * quant_error;
+        this.temp[x + (y+1) * 87] = this.temp[x + (y + 1) * 87] + (5.0 / 16) * quant_error;
+        this.temp[x+1 + (y+1) * 87] = this.temp[x + 1 + (y + 1) * 87] + (1.0 / 16) * quant_error;
+
+        this.result.push(newpixel);
+
+      }
+    }
+
+    return this.result;
+  }
+
+  bayer() {
+
+    this.temp = createGraphics(87, 87);
+    this.temp.image(this.src, 0, 0);
+
+    this.result = [];
+
+    this.temp.loadPixels();
+
+    for (let y = 0; y < 87; y++) {
+      for (let x = 0; x < 87; x++) {
+
+        let oldpixel = this.temp.get(x, y);
+        let value = color(
+          brightness(oldpixel) +
+            this.mratio * this.matrix[x % 4][y % 4] * this.mfactor
+        );
+        let newpixel = this.makeBlackorWhite(value);
+        this.temp.set(x, y, color(newpixel));
+
+        this.result.push(newpixel);
+
+      }
+    }
+    this.temp.updatePixels();
+
+    return this.result;
+  }
+
+  atkinson() {
+
+    this.result = [];
+
+    for (let y = 0; y < 87; y++) {
+      for (let x = 0; x < 87; x++) {
+
+        let oldpixel = this.temp[x + y * 87];
+
+        let newpixel = this.makeBlackorWhite(oldpixel);
+        let quant_error = brightness(oldpixel) - brightness(color(newpixel));
+
+        this.temp.push(newpixel);
+        
+        this.temp[x+1 + y * 87] = brightness(this.src.get(x+1, y)) + (1.0 / 8) * quant_error;
+        this.temp[x-1 + (y+1) * 87] = brightness(this.src.get(x-1, y+1)) + (1.0 / 8) * quant_error;
+        this.temp[x + (y+1) * 87] = brightness(this.src.get(x, y+1)) + (1.0 / 8) * quant_error;
+        this.temp[x+1 + (y+1) * 87] = brightness(this.src.get(x+1, y+1)) + (1.0 / 8) * quant_error;
+        this.temp[x+2 + y * 87] = brightness(this.src.get(x+2, y)) + (1.0 / 8) * quant_error;
+        this.temp[x + (y+2) * 87] = brightness(this.src.get(x, y + 2)) + (1.0 / 8) * quant_error;
+
+        this.result.push(newpixel);
+
+      }
+    }
+
+    return this.result;
+  }
+
+  rand() {
+    
+    this.temp = createGraphics(this.src.width, this.src.height);
+    this.temp.image(this.src, 0, 0);
+
+    this.result = [];
+
+    let s = 1;
+    this.temp.loadPixels();
+    
+    for (let y = 0; y < 87; y++) {
+      for (let x = 0; x < 87; x++) {
+
+        // let index = 4 * (x + y * 87);
+        let index = x + y * 87;
+
+        let oldpixel = this.temp.get(x, y);
+        let newpixel = this.makeBlackorWhite(
+          color(brightness(oldpixel) + random(-64, 64))
+        );
+        this.temp.set(x, y, newpixel);        
+        
+        this.result.push(newpixel);
+      }
+    }
+    this.temp.updatePixels();
+
+    return this.result;
+  }
+
+  makeBlackorWhite(pixelColor) {
+    let b = brightness(pixelColor);
+    if (b > 128) {
+      // Since brightness in HSB is from 0 to 255
+      return 255; // White
+    } else {
+      return 0; // Black
+    }
+  }
+}
---
+++ P5js_Living_QR_Code/sketch.js
@@ -0,0 +1,245 @@
+/*
+ * @name Living QR code
+ * @Jaap7M! Video from the user’s built-in webcam is showing in this QR code!
+ * @description A QR code that appears to be entirely composed of your webcam video — and yet remains scannable!
+ * <br>Read more on this project (including how to do this in Processing) on Github: <a href="https://github.com/twisst/living_QR_code"></a>
+ */
+
+// const a = performance.now();
+// console.log('start');
+
+// These hold the QR code
+let blank, dataImageFile;
+
+// QR code image, in this case one that leads to the Github page for this project.
+let dataImageFilename = "qrcode_github-livingQR-repo.png";
+
+let video;
+let showVideo = true;
+let showNoise = true;
+let showDither = true;
+let showFullQR = false;
+
+let squareSize;
+let subsquareSize;
+let totalQRsize = 0;
+let vmargin;
+let hmargin;
+
+// set noiseLevel:
+// there will be a 1 in noiseLevel chance that an extra subsquare is drawn around the module center subsquare.
+let noiseLevel = 3;
+
+let required = new Array(841); // required parts of the 29x29 QR code (that should appear as large solid squares)
+let data = new Array(841); // will hold the actual QR code
+
+let d;
+let mode = 1;
+let p;
+
+let f, s;
+let arrow;
+
+// loading QR module colors from a PNG
+function loadQRcode(png) {
+  let modules = new Array(841);
+  png.loadPixels(); // only has black and white
+  for (let y = 0; y < png.height; y++) {
+    for (let x = 0; x < png.width; x++) {
+      let loc = x + y * png.width;
+      let c = png.get(x, y); // krijg de kleur van de pixel op (x, y)
+      let brightnessValue = brightness(c); // krijg de helderheid van de kleur
+      modules[loc] = brightnessValue;
+    }
+  }
+  return modules;
+}
+
+function preload() {
+  blank = loadImage("blank_qr_code.png");
+  dataImageFile = loadImage(dataImageFilename);
+  arrow = loadImage("arrow.png");
+  f = "Verdana";
+  s = loadFont("ka1.ttf"); // font: Karmatic arcade by Vic Fieger
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  colorMode(HSB, 255);
+
+  d = new Dither();
+
+  // Size of each 'module' in the grid
+  squareSize = 3;
+  // squareSize should be a multiple of 3 so it is neatly dividble into 3x3 sub-squares.
+  // Since this number is the basis for the rest of the dimensions, it determines the scale of the QR code.
+  // We should use this instead of scale(), because that function gives rational pixel numbers and that
+  // creates unwanted white lines between squares.
+
+  // Now we know the value of width and height, so we can scale the QR code to the size of the screen.
+  // here we use 29 + 8 in order to have a margin around the QR code (the poetically named 'quiet zone')
+  while ((squareSize + 3) * 37 < height && (squareSize + 3) * 37 < width) {
+    squareSize += 3;
+  }
+  console.log("squareSize is ", squareSize);
+
+  totalQRsize = squareSize * 29; // length of the sides of the whole QR code
+  subsquareSize = squareSize / 3; // we divide the modules of the QR code into 9 sub-squares that have sides 1/3 of the larger square.
+  vmargin = (height - totalQRsize) / 2; // vertical margin
+  hmargin = (width - totalQRsize) / 2; // horizontal margin
+
+  required = loadQRcode(blank); // image with just the required squares
+  data = loadQRcode(dataImageFile); // image with the working QR code
+
+  // take in webcam video
+  video = createCapture(VIDEO,{ flipped:true });
+  video.size(640, 360);
+  video.hide();
+
+  frameRate(4);
+
+  noStroke();
+  
+  // const b = performance.now();
+  // console.log('Setup done; it took ' + (b - a) + ' ms.');
+  
+}
+
+function draw() {
+  background(255);
+  translate(hmargin - 50, vmargin);
+  
+  // const k = performance.now();
+  // console.log('Start draw');
+
+  // Text with an arrow
+  fill(0);
+  textFont(s, 28);
+  text("You\nare\nwhat\nyou\nscan.", totalQRsize * 1.1, totalQRsize * 0.05);
+  textFont(f, 28);
+  text("Scan yourself!", totalQRsize * 1.1, totalQRsize * 0.7);
+  image(
+    arrow,
+    totalQRsize * 1.1,
+    totalQRsize * 0.7,
+    arrow.width / 2,
+    arrow.height / 2
+  );
+
+
+  // CAPTURE VIDEO
+
+  if (video.loadedmetadata) {
+
+    // Get a portion of the loaded image
+    let videoSize = 348; // needs to be a multiple of 87 (29 squares x 3 subsquares) so that is easier to fit over the QR code.
+    let frame = video.get(146, 0, videoSize, videoSize); // the crop starts with a 146 left margin to center the video
+    frame.loadPixels();
+    frame.resize(87, 87);
+
+    // SHOW VIDEO
+
+    if (showDither) {  // dither the video
+      d.feed(frame);
+      frame = d.dither(mode);
+    }
+
+    if (showVideo) { // Draw video frame (either dithered or not)
+      // Begin loop for rows
+      for (let i = 1; i < 87; i++) { // 87 because that's how many columns and rows of subsquares there are
+        // Begin loop for columns
+        for (let j = 1; j < 87; j++) {
+          
+          // Where are we, pixel-wise, on the video frame?
+          let xv = i * subsquareSize; // each square consists of 3x3 smaller squares of 4x4 pixels
+          let yv = j * subsquareSize;
+          
+          // find the video pixel for the current square
+          let loc = i + j * 87;
+          
+          // Each square is a shade of grey value depending on video brightness
+          if (showDither) {
+            let c = frame[loc];
+            fill(c);
+          } else {
+            let c = frame.get(i, j); // get color
+            fill(brightness(c));
+          }
+          square(xv, yv, subsquareSize); // draw squares based on the video
+        }
+      }
+    }
+
+    // SHOW QR CODE
+
+    // no need to consider the subsquares here, so just 29x29 (instead of 87x87 for the video)
+    for (let row = 0; row < 29; row++) {
+      for (let col = 0; col < 29; col++) {
+
+        // Where are we, pixel-wise?
+        let x = row * squareSize;
+        let y = col * squareSize;
+        let loc = row * 29 + col;
+
+        if (required[loc] != 128) {
+          // if this square is part of the required base pattern, it needs to be bigger.
+
+          fill(color(required[loc]));
+          square(x, y, squareSize);
+
+        } else {
+          // otherwise, this is one of the smaller squares
+
+          if (showNoise) {
+            
+            // random dots
+            if (round(random(noiseLevel)) == 1) { // determines how often we draw an extra square
+              fill(round(random(1)) * 255); // black or white
+              // choose one random square to fill (might be the center one, but that will be overwritten below)
+              square(
+                x + round(random(0, 2)) * subsquareSize,
+                y + round(random(0, 2)) * subsquareSize,
+                subsquareSize
+              );
+            }
+            
+          }
+          
+          fill(data[loc]);
+          // the data parts of the QR code only have to be 1/12th of the larger squares to still be scan-able.
+          if (!showFullQR) {
+            // margins on both the x and y because this is the center square of the larger 9x9 grid
+            square(x + subsquareSize, y + subsquareSize, subsquareSize);
+          } else {
+            // no margins, just show the full square
+            square(x, y, squareSize);
+          }
+        }
+      }
+    } // end of showing QR code
+  }
+
+  // const l = performance.now();
+  // console.log('Drawing took ' + (l - k) + ' ms.');
+  
+} // end of draw()
+
+function keyPressed() {  // 1 toggle video, 2 toggle noise, 3 toggle full QR code, 4 switch dithering modes
+  if (key == "1") {
+    showVideo = !showVideo;
+  } else if (key == "2") {
+    showNoise = !showNoise;
+  } else if (key == "3") {
+    showFullQR = !showFullQR;
+  } else if (key == "4") {
+    mode = (mode + 1) % 5; // 0 floyd_steinberg, 1 bayer, 2 atkinson, 3 random, 4 no dithering
+    console.log("Dither mode", mode);
+    if (mode == 4) {
+      showDither = false; // last option is turn dithering off
+    } else {
+      showDither = true;
+      showVideo = true;
+    }
+  }
+}
